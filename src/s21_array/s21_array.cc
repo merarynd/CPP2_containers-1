23c58594639f66ed6cbbe9dcb6dd14f54fa7e13f
Mera Ryndoon <merarynd@student.21-school.ru>
2024-02-19 20:12:01 +0300

    array.h

--- src/s21_array/s21_array.cc
+++ src/s21_array/s21_array.cc
@@ -1,1 +1,28 @@
 #include "s21_array.h"
+
+array::array() {}
+array::array(std::initializer_list<value_type> const& items) {}
+array::array(const array& a) {}
+array::array(array&& a) {}
+array::~array() {}
+
+// Array Element access:
+array::operator=(array && a) {}
+reference array::at(size_type pos) {}
+reference array::operator[](size_type pos) {}
+const_reference array::front() {}
+const_reference array::back() {}
+iterator array::data() {}
+
+// Array Iterators:
+array::iterator begin() {}
+array::iterator end() {}
+
+// Array Capacity:
+bool array::empty() {}
+size_type array::size() {}
+size_type array::max_size() {}
+
+// Array Modifiers:
+void array::swap(array& other) {}
+void array::fill(const_reference value) {}
--- src/s21_array/s21_array.h
+++ src/s21_array/s21_array.h
@@ -1,5 +1,47 @@
 #ifndef S21_ARREY_H_
 #define S21_ARREY_H_
 
-class Arrey {};
+namespace s21 {
+template <typename T, std::size_t N>
+class arrey {
+ public:
+  using value_type = T;
+  using reference = T&;
+  using const_reference = const T&;
+  using iterator = T*;
+  using const_iterator = const T*;
+  using size_type = size_t;
+
+  // Array Member functions:
+  array();
+  array(std::initializer_list<value_type> const& items);
+  array(const array& a);
+  array(array&& a);
+  ~array();
+
+  // Array Element access:
+  operator=(array && a);
+  reference at(size_type pos);
+  reference operator[](size_type pos);
+  const_reference front();
+  const_reference back();
+  iterator data();
+
+  // Array Iterators:
+  iterator begin();
+  iterator end();
+
+  // Array Capacity:
+  bool empty();
+  size_type size();
+  size_type max_size();
+
+  // Array Modifiers:
+  void swap(array& other);
+  void fill(const_reference value);
+
+ private:
+  double** matrix_ = nullptr;
+};
+}  // namespace s21
 #endif  // S21_ARREY_H_
